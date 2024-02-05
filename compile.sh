7aa53d3d4672ad2dd33d285eb2e5dcd05850eff1
Evan Bowman <evanbowman@Evans-MacBook-Air.local>
2024-02-04 19:42:39 -0500

    add compilation script

---
+++ compile.sh
@@ -0,0 +1,2 @@
+python3 tools/encode_files.py
+cat SkylandEngine.gba fs.bin > Skyland.gba
--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
-
+echo Fetching rom...
 curl https://skylandgame.io/beta/download > Skyland.gba
+echo Unpacking rom...
 python3 unpack_rom.py
+echo "Done! Now you can edit any of the game's scripts, \nand then run compile.sh to rebuild Skyland.gba!"
